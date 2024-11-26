35a09014051b7bbd335f3b147345aceb8ff9e7c9
maamedcadeh1 <mohamedcaska73@gmail.com>
2024-11-26 20:16:00 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,11 +5,11 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "EC1l1RKZ#iIk20jIp2r7-3tmLs0h2qfqTCnPfenAgsis_CVXc-3w",
+SESSION_ID: process.env.SESSION_ID || "HmQ1yTKR#rnz-uod4ojxwbSmQDLVmAGF7ke1ihvMzBGgdSd2bToc",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/n5vvij.jpg",
 SUDO_NB: process.env.SUDO_NB || "263780934873",
 MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-MODE: process.env.MODE || "public",
+MODE: process.env.MODE || "private",
 AUTO_VOICE: "true"
 };
